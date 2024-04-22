b0c76272bb730a12adfede9857486159d5394123
George Cancino <georgecancino@hotmail.com>
2024-04-22 15:05:17 -0500

    Add username page

--- config/routes.rb
+++ config/routes.rb
@@ -8,4 +8,6 @@ Rails.application.routes.draw do
   # Defines the root path route ("/")
   # root "posts#index"
   root 'pages#index'
+
+  get 'georgecancino', to: 'pages#georgecancino', as: 'georgecancino'
 end
