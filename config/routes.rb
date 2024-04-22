ac453f73386ff23e9647c6e424b2e1ecba520aa5
Leah Bennie <lcbennie05@gmail.com>
2024-04-22 15:08:12 -0500

    fixed mild format error

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,6 @@
 Rails.application.routes.draw do
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
-
+  get 'leahcb05', to: 'pages#leahcb05', as: 'leahcb05'
   # Reveal health status on /up that returns 200 if the app boots with no exceptions, otherwise 500.
   # Can be used by load balancers and uptime monitors to verify that the app is live.
   get "up" => "rails/health#show", as: :rails_health_check
@@ -9,5 +9,3 @@ Rails.application.routes.draw do
   # root "posts#index"
   root 'pages#index'
 end
-
-get 'leahcb05', to 'pages#leahcb05', as: 'leahcb05'
